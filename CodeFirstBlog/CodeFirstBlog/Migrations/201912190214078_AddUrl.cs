b0cff01cee9ee4cf9ce469cc6d80e0cddaf53daa
Talbot French <talbot.french@gmail.com>
2019-12-18 19:25:02 -0700

    add CodeFirstBlog

---
+++ CodeFirstBlog/CodeFirstBlog/Migrations/201912190214078_AddUrl.cs
@@ -0,0 +1,53 @@
+﻿namespace CodeFirstBlog.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddUrl : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Blogs",
+                c => new
+                    {
+                        BlogId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        URL = c.String(),
+                    })
+                .PrimaryKey(t => t.BlogId);
+            
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        PostId = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Content = c.String(),
+                        BlogId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.PostId)
+                .ForeignKey("dbo.Blogs", t => t.BlogId, cascadeDelete: true)
+                .Index(t => t.BlogId);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserName = c.String(nullable: false, maxLength: 128),
+                        display_name = c.String(),
+                    })
+                .PrimaryKey(t => t.UserName);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Posts", "BlogId", "dbo.Blogs");
+            DropIndex("dbo.Posts", new[] { "BlogId" });
+            DropTable("dbo.Users");
+            DropTable("dbo.Posts");
+            DropTable("dbo.Blogs");
+        }
+    }
+}
